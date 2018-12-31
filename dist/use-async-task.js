b3ba402a2df1168f7a8d182878c00c0440313792
daishi <daishi@axlight.com>
2018-12-31 09:41:35 +0900

    v0.5.1

--- dist/use-async-task.js
+++ dist/use-async-task.js
@@ -5,8 +5,6 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = exports.useAsyncTask = void 0;
 
-require("core-js/modules/es6.array.is-array");
-
 require("core-js/modules/es6.array.for-each");
 
 require("core-js/modules/es6.array.filter");
@@ -29,14 +27,6 @@ require("core-js/modules/es6.symbol");
 
 var _react = require("react");
 
-function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
-
-function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
-
-function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
-
-function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
-
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
@@ -44,6 +34,14 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
 function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
 
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+var forcedReducer = function forcedReducer(state) {
+  return !state;
+};
+
+var useForceUpdate = function useForceUpdate() {
+  return (0, _react.useReducer)(forcedReducer, false)[1];
+};
 
 var idCounter = 0;
 
@@ -129,23 +127,19 @@ var createTask = function createTask(func, notifyUpdate) {
 };
 
 var useAsyncTask = function useAsyncTask(func, inputs) {
-  var task = (0, _react.useRef)(null);
-  task.current = (0, _react.useMemo)(function () {
-    return createTask(func, function (t) {
-      if (task.current && task.current.taskId === t.taskId) {
-        // eslint-disable-next-line no-use-before-define
-        setState(t);
+  var forceUpdate = useForceUpdate();
+  var task = (0, _react.useRef)({});
+  var newTask = (0, _react.useMemo)(function () {
+    return createTask(func, function (updatedTask) {
+      if (task.current && task.current.taskId === updatedTask.taskId) {
+        task.current = updatedTask;
+        forceUpdate();
       }
     });
   }, inputs);
 
-  var _useState = (0, _react.useState)(task.current),
-      _useState2 = _slicedToArray(_useState, 2),
-      state = _useState2[0],
-      setState = _useState2[1];
-
-  if (state.taskId !== task.current.taskId) {
-    setState(task.current);
+  if (task.current && task.current.taskId !== newTask.taskId) {
+    task.current = newTask;
   }
 
   (0, _react.useEffect)(function () {
@@ -155,7 +149,7 @@ var useAsyncTask = function useAsyncTask(func, inputs) {
 
     return cleanup;
   }, []);
-  return state;
+  return task.current;
 };
 
 exports.useAsyncTask = useAsyncTask;
