ea055606b28c3b3e318198a16953743276bcecec
daishi <daishi@axlight.com>
2019-01-04 10:18:34 +0900

    another example for start button

--- examples/03_startbutton/src/App.tsx
+++ examples/03_startbutton/src/App.tsx
@@ -1,16 +1,26 @@
 import * as React from 'react';
 
 import DisplayRemoteData from './DisplayRemoteData';
+import UserInfo from './UserInfo';
 
 const { useState } = React;
 
 const App = () => {
   const [id, setId] = useState('1');
+  const [id2, setId2] = useState('1');
   return (
     <div>
-      id:
-      <input value={id} onChange={e => setId(e.target.value)} />
-      {id && <DisplayRemoteData id={id} />}
+      <div>
+        id:
+        <input value={id} onChange={e => setId(e.target.value)} />
+        {id && <DisplayRemoteData id={id} />}
+      </div>
+      <hr />
+      <div>
+        id:
+        <input value={id2} onChange={e => setId2(e.target.value)} />
+        {id2 && <UserInfo id={id2} />}
+      </div>
     </div>
   );
 };
---
+++ examples/03_startbutton/src/UserInfo.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+
+import { useAsyncTaskFetch } from 'react-hooks-async';
+
+const Err: React.SFC<{ error: Error }> = ({ error }) => (
+  <div>
+    Error:
+    {error.name}
+    {error.message}
+  </div>
+);
+
+const Loading: React.SFC<{ abort: () => void }> = ({ abort }) => (
+  <div>
+    Loading...
+    <button type="button" onClick={abort}>Abort</button>
+  </div>
+);
+
+type Result = {
+  data: {
+    first_name: string;
+  };
+};
+
+const UserInfo: React.FC<{ id: string }> = ({ id }) => {
+  const url = `https://reqres.in/api/users/${id}?delay=1`;
+  const asyncTask = useAsyncTaskFetch<Result>(url);
+  const {
+    started,
+    pending,
+    error,
+    result,
+    abort,
+  } = asyncTask;
+  if (!started) return <button onClick={asyncTask.start}>start</button>;
+  if (error) return <Err error={error} />;
+  if (pending) return <Loading abort={abort} />;
+  if (!result) return <div>No result</div>;
+  return (
+    <div>
+      First Name:
+      {result.data.first_name}
+    </div>
+  );
+};
+
+export default UserInfo;
