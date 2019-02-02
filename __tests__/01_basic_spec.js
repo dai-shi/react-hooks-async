c3ea421f629472705cefc6c57b34fe38ee1ccaee
daishi <daishi@axlight.com>
2019-02-02 23:18:31 +0900

    WIP: use react-testing-library

--- __tests__/01_basic_spec.js
+++ __tests__/01_basic_spec.js
@@ -1,20 +1,55 @@
-/* eslint-env jest */
+import React from 'react';
+import {
+  render,
+  flushEffects,
+  cleanup,
+} from 'react-testing-library';
 
-// import React from 'react';
-// import { configure, mount } from 'enzyme';
-// import toJson from 'enzyme-to-json';
-// import Adapter from 'enzyme-adapter-react-16';
+import { useAsyncTask, useAsyncTaskTimeout, useAsyncRun } from '../src/index';
 
-import { useAsyncTask } from '../src/index';
-
-// configure({ adapter: new Adapter() });
+jest.useFakeTimers();
 
 describe('basic spec', () => {
+  afterEach(cleanup);
+
   it('should have a function', () => {
     expect(useAsyncTask).toBeDefined();
   });
 
-  it.skip('should create a component', () => {
-    // TODO
+  it.skip('should create a component with timeout', () => {
+    const Waiting = ({ abort }) => (
+      <div>
+        Waiting...
+        <button type="button" onClick={abort}>Abort</button>
+      </div>
+    );
+    const renderHi = () => <h1>Hi</h1>;
+    const DelayedMessage = ({ delay }) => {
+      const asyncTask = useAsyncTaskTimeout(renderHi, delay);
+      useAsyncRun(asyncTask);
+      const {
+        pending,
+        error,
+        result,
+        abort,
+      } = asyncTask;
+      if (error) return <div>Error:{error.message}</div>;
+      if (pending) return <Waiting abort={abort} />;
+      return <div>Result:{result}</div>;
+    };
+    const App = () => (
+      <div>
+        <DelayedMessage delay={3000} />
+        <DelayedMessage delay={1000} />
+      </div>
+    );
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+    jest.advanceTimersByTime(1000);
+    flushEffects();
+    expect(container).toMatchSnapshot();
+    jest.advanceTimersByTime(2000);
+    flushEffects();
+    expect(container).toMatchSnapshot();
   });
 });
