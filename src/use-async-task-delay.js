58d5b39771f11e3177b3cf455628c2e6f04948c7
daishi <daishi@axlight.com>
2019-02-02 19:53:30 +0900

    do not rely on useMemo

--- src/use-async-task-delay.js
+++ src/use-async-task-delay.js
@@ -1,10 +1,18 @@
-import { useCallback } from 'react';
+import { useRef } from 'react';
+import shallowequal from 'shallowequal';
 
 import { useAsyncTaskTimeout } from './use-async-task-timeout';
 
-export const useAsyncTaskDelay = (milliSeconds, inputs) => useAsyncTaskTimeout(
-  useCallback(() => true, inputs),
-  milliSeconds,
-);
+const emptyInputs = Symbol('emtpy inputs');
+
+export const useAsyncTaskDelay = (milliSeconds, inputs) => {
+  const func = useRef();
+  const previousInputs = useRef(emptyInputs);
+  if (!shallowequal(inputs, previousInputs.current)) {
+    previousInputs.current = inputs;
+    func.current = () => true;
+  }
+  return useAsyncTaskTimeout(func.current, milliSeconds);
+};
 
 export default useAsyncTaskDelay;
--- src/use-async-task.js
+++ src/use-async-task.js
@@ -1,9 +1,9 @@
 import {
   useEffect,
-  useMemo,
   useReducer,
   useRef,
 } from 'react';
+import shallowequal from 'shallowequal';
 
 const forcedReducer = state => !state;
 const useForceUpdate = () => useReducer(forcedReducer, false)[1];
@@ -48,17 +48,20 @@ const createTask = (func, notifyUpdate) => {
   return task;
 };
 
+const emptyInputs = Symbol('emtpy inputs');
+
 export const useAsyncTask = (func, inputs) => {
   const forceUpdate = useForceUpdate();
-  const task = useRef({});
-  const newTask = useMemo(() => createTask(func, (updatedTask) => {
-    if (task.current && task.current.taskId === updatedTask.taskId) {
-      task.current = updatedTask;
-      forceUpdate();
-    }
-  }), inputs);
-  if (task.current && task.current.taskId !== newTask.taskId) {
-    task.current = newTask;
+  const task = useRef();
+  const previousInputs = useRef(emptyInputs);
+  if (!shallowequal(inputs, previousInputs.current)) {
+    previousInputs.current = inputs;
+    task.current = createTask(func, (updatedTask) => {
+      if (task.current && task.current.taskId === updatedTask.taskId) {
+        task.current = updatedTask;
+        forceUpdate();
+      }
+    });
   }
   useEffect(() => {
     const cleanup = () => {
