42886fb4db2704b22dea727f2dcdc7cebb54bc81
daishi <daishi@axlight.com>
2019-08-14 08:52:22 +0900

    run compile

---
+++ dist/utils.js
@@ -0,0 +1,30 @@
+"use strict";
+
+require("core-js/modules/es.array.some");
+
+require("core-js/modules/es.object.define-property");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.useMemoList = void 0;
+
+var _react = require("react");
+
+var useMemoList = function useMemoList(list) {
+  var compareFn = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : function (a, b) {
+    return a === b;
+  };
+  var listRef = (0, _react.useRef)(list);
+  var listChanged = list.length !== listRef.current.length || list.some(function (arg, index) {
+    return !compareFn(arg, listRef.current[index]);
+  });
+  (0, _react.useEffect)(function () {
+    if (listChanged) {
+      listRef.current = list;
+    }
+  });
+  return listChanged ? list : listRef.current;
+};
+
+exports.useMemoList = useMemoList;
